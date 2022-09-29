e6e7f60baee39cd45c67508c8191f36e55db3270
Joanna D <joanna.debek.a@gmail.com>
2022-09-29 15:07:03 +0200

    comment add and delete

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post), status: :see_other
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:user, :content)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_29_094332) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_29_122259) do
+  create_table "comments", force: :cascade do |t|
+    t.string "user"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
